b009cb5df2b5a097f0c92029fe70c890c8cbdcb7
kondi0 <Kondi.czerwinski@gmail.com>
2017-10-31 11:53:46 +0100

    [ng4-intl-phone] - Update demo presentation

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -1,35 +1,52 @@
 import {Component, OnInit} from '@angular/core';
-import { CountryService } from 'quickstart-lib';
+import {CountryService} from 'quickstart-lib';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'demo-app',
-  template: `
-    <int-phone-prefix [(ngModel)]="phoneValue"></int-phone-prefix>
-    <h3>The phone number is: {{phoneValue}}</h3>
+    selector: 'demo-app',
+    template: `
+<div class="row mc">
+<div class="card">
+    <div class="row">
+        <div class="col-md-6">
+            <int-phone-prefix [(ngModel)]="phoneValue"></int-phone-prefix>
+        </div>
+        <div class="col-md-6">
+            <span>The phone number is: {{phoneValue}}</span>
+        </div>
+    </div>
     <form [formGroup]="myForm" (ngSubmit)="logForm(myForm.value)" novalidate>
-    <int-phone-prefix [locale]="'es'"
+        <div class="row mt">
+            <div class="col-md-10">
+                <int-phone-prefix [locale]="'es'"
                     formControlName="myPhone"></int-phone-prefix>
-                    <button class="btn btn-main">Submit</button>
-</form>
+            </div>
+            <div class="col-md-2">
+               <button class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </form>
+    </div>
+</div>
   `,
 })
-export class AppComponent implements OnInit{
-  phoneValue: string;
-  private myForm: FormGroup;
-  constructor(libService: CountryService,
-              private formBuilder: FormBuilder) {
-    this.phoneValue = '123345456';
-  }
+export class AppComponent implements OnInit {
+    phoneValue: string;
+    private myForm: FormGroup;
+
+    constructor(libService: CountryService,
+                private formBuilder: FormBuilder) {
+        this.phoneValue = '123345456';
+    }
 
 
-  ngOnInit() {
-    this.myForm = this.formBuilder.group({
-      myPhone: ['', Validators.required]
-    });
-  }
+    ngOnInit() {
+        this.myForm = this.formBuilder.group({
+            myPhone: ['', Validators.required]
+        });
+    }
 
-  logForm(value: any) {
-    console.log(value);
-  }
+    logForm(value: any) {
+        console.log(value);
+    }
 }
