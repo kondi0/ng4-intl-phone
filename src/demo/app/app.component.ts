93656812b72f54c472b0d3c6eab371882c22366e
kondi0 <Kondi.czerwinski@gmail.com>
2017-10-31 11:15:35 +0100

    [ng4-intl-phone] - Prevent auto submit on flag select

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CountryService } from 'quickstart-lib';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'demo-app',
   template: `
-    <int-phone-prefix [(ngModel)]="meaning"></int-phone-prefix>
-    <h3>Meaning is: {{meaning}}</h3>
+    <int-phone-prefix [(ngModel)]="phoneValue"></int-phone-prefix>
+    <h3>The phone number is: {{phoneValue}}</h3>
+    <form [formGroup]="myForm" (ngSubmit)="logForm(myForm.value)" novalidate>
+    <int-phone-prefix [locale]="'es'"
+                    formControlName="myPhone"></int-phone-prefix>
+                    <button class="btn btn-main">Submit</button>
+</form>
   `,
 })
-export class AppComponent {
-  meaning: any;
-  constructor(libService: CountryService) {
-    this.meaning = '123345456';
+export class AppComponent implements OnInit{
+  phoneValue: string;
+  private myForm: FormGroup;
+  constructor(libService: CountryService,
+              private formBuilder: FormBuilder) {
+    this.phoneValue = '123345456';
+  }
+
+
+  ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      myPhone: ['', Validators.required]
+    });
+  }
+
+  logForm(value: any) {
+    console.log(value);
   }
 }
--- src/demo/app/app.module.ts
+++ src/demo/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { InternationalPhoneModule } from 'quickstart-lib';
 
 import { AppComponent }  from './app.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  imports:      [ BrowserModule, InternationalPhoneModule, FormsModule],
+  imports:      [
+    BrowserModule,
+    InternationalPhoneModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
