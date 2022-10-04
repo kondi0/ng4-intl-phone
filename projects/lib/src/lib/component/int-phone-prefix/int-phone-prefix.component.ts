633fd0b0af40a055879a8c03730a9573947b48c0
KonradCzerwinski <kondi.czerwinski@gmail.com>
2022-10-04 16:35:19 +0100

    angular 14 migration

--- projects/lib/src/lib/component/int-phone-prefix/int-phone-prefix.component.ts
+++ projects/lib/src/lib/component/int-phone-prefix/int-phone-prefix.component.ts
@@ -14,7 +14,6 @@ const COUNTER_CONTROL_ACCESSOR = {
 const PLUS = '+';
 
 @Component({
-    moduleId: module.id,
     selector: 'int-phone-prefix',
     templateUrl: './int-phone-prefix.component.html',
     styleUrls: [
